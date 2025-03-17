d74e971fee56ed07b81eb2253037a1612eec3255
Рома Сандаков <r.sandakov@inbox.ru>
2025-03-17 05:21:17 +0200

    Добавьте файлы проекта.

---
+++ MyToode/Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace MyToode
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtQuantity = new System.Windows.Forms.TextBox();
+            this.txtPrice = new System.Windows.Forms.TextBox();
+            this.txtCategory = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnSelectImage = new System.Windows.Forms.Button();
+            this.txtImagePath = new System.Windows.Forms.TextBox();
+            this.pictureBoxPreview = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxPreview)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(54, 277);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(683, 150);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(132, 30);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 20);
+            this.txtName.TabIndex = 1;
+            // 
+            // txtQuantity
+            // 
+            this.txtQuantity.Location = new System.Drawing.Point(132, 65);
+            this.txtQuantity.Name = "txtQuantity";
+            this.txtQuantity.Size = new System.Drawing.Size(100, 20);
+            this.txtQuantity.TabIndex = 2;
+            // 
+            // txtPrice
+            // 
+            this.txtPrice.Location = new System.Drawing.Point(132, 101);
+            this.txtPrice.Name = "txtPrice";
+            this.txtPrice.Size = new System.Drawing.Size(100, 20);
+            this.txtPrice.TabIndex = 3;
+            // 
+            // txtCategory
+            // 
+            this.txtCategory.Location = new System.Drawing.Point(132, 172);
+            this.txtCategory.Name = "txtCategory";
+            this.txtCategory.Size = new System.Drawing.Size(100, 20);
+            this.txtCategory.TabIndex = 5;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(126, 216);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(106, 43);
+            this.btnAdd.TabIndex = 6;
+            this.btnAdd.Text = "Lisa";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(248, 216);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(106, 43);
+            this.btnDelete.TabIndex = 7;
+            this.btnDelete.Text = "Kustuta";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(74, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Nimi";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(74, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Kogus";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(74, 101);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Hind";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(74, 136);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(21, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Pilt";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(74, 172);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(58, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Kategooria";
+            // 
+            // btnSelectImage
+            // 
+            this.btnSelectImage.Location = new System.Drawing.Point(248, 136);
+            this.btnSelectImage.Name = "btnSelectImage";
+            this.btnSelectImage.Size = new System.Drawing.Size(75, 23);
+            this.btnSelectImage.TabIndex = 13;
+            this.btnSelectImage.Text = "button1";
+            this.btnSelectImage.UseVisualStyleBackColor = true;
+            this.btnSelectImage.Click += new System.EventHandler(this.btnSelectImage_Click);
+            // 
+            // txtImagePath
+            // 
+            this.txtImagePath.Location = new System.Drawing.Point(132, 139);
+            this.txtImagePath.Name = "txtImagePath";
+            this.txtImagePath.Size = new System.Drawing.Size(100, 20);
+            this.txtImagePath.TabIndex = 14;
+            // 
+            // pictureBoxPreview
+            // 
+            this.pictureBoxPreview.Location = new System.Drawing.Point(543, 12);
+            this.pictureBoxPreview.Name = "pictureBoxPreview";
+            this.pictureBoxPreview.Size = new System.Drawing.Size(194, 247);
+            this.pictureBoxPreview.TabIndex = 15;
+            this.pictureBoxPreview.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBoxPreview);
+            this.Controls.Add(this.txtImagePath);
+            this.Controls.Add(this.btnSelectImage);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtCategory);
+            this.Controls.Add(this.txtPrice);
+            this.Controls.Add(this.txtQuantity);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxPreview)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtQuantity;
+        private System.Windows.Forms.TextBox txtPrice;
+        private System.Windows.Forms.TextBox txtCategory;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnSelectImage;
+        private System.Windows.Forms.TextBox txtImagePath;
+        private System.Windows.Forms.PictureBox pictureBoxPreview;
+    }
+}
+
---
+++ MyToode/Form1.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace MyToode
+{
+    public partial class Form1 : Form
+    {
+        private string connectionString = "Data Source=(localdb)\\mssqllocaldb;Initial Catalog=Pood;Integrated Security=True";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataGridView1.CellClick += dataGridView1_CellContentClick;
+            SetupDataGridView();
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                SqlDataAdapter adapter = new SqlDataAdapter("SELECT * FROM Toode", conn);
+                DataTable table = new DataTable();
+                adapter.Fill(table);
+
+                // Выводим все имена столбцов для проверки
+                foreach (DataColumn column in table.Columns)
+                {
+                    Console.WriteLine($"Column: {column.ColumnName}");
+                }
+
+                // Добавляем колонку для изображений, если её нет
+                if (!table.Columns.Contains("pilt_image"))
+                {
+                    table.Columns.Add("pilt_image", typeof(Image));
+                }
+
+                foreach (DataRow row in table.Rows)
+                {
+                    string imagePath = row["pilt"]?.ToString(); // Используем безопасное обращение
+                    if (!string.IsNullOrEmpty(imagePath) && File.Exists(imagePath))
+                    {
+                        using (var imgStream = new FileStream(imagePath, FileMode.Open, FileAccess.Read))
+                        {
+                            row["pilt_image"] = Image.FromStream(imgStream);
+                        }
+                    }
+                    else
+                    {
+                        row["pilt_image"] = new Bitmap(100, 100); // Заглушка, если файла нет
+                    }
+                }
+
+                dataGridView1.DataSource = table;
+            }
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtName.Text) ||
+                string.IsNullOrWhiteSpace(txtQuantity.Text) ||
+                string.IsNullOrWhiteSpace(txtPrice.Text) ||
+                string.IsNullOrWhiteSpace(txtCategory.Text) ||
+                pictureBoxPreview.Image == null)
+            {
+                MessageBox.Show("Заполните все поля и выберите изображение!");
+                return;
+            }
+
+            // Копируем изображение в папку проекта
+            string imagesFolder = Path.Combine(Application.StartupPath, "images");
+            Directory.CreateDirectory(imagesFolder);
+
+            string fileName = Path.GetFileName(txtImagePath.Text);
+            string newImagePath = Path.Combine(imagesFolder, fileName);
+
+            // Проверяем, существует ли файл уже, если нет - копируем
+            if (!File.Exists(newImagePath))
+            {
+                File.Copy(txtImagePath.Text, newImagePath);
+            }
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand(
+                    "INSERT INTO Toode (toodenimi, kogus, hind, pilt, kategooria) VALUES (@toodenimi, @kogus, @hind, @pilt, @kategooria)", conn);
+                cmd.Parameters.AddWithValue("@toodenimi", txtName.Text);
+                cmd.Parameters.AddWithValue("@kogus", int.Parse(txtQuantity.Text));
+                cmd.Parameters.AddWithValue("@hind", decimal.Parse(txtPrice.Text));
+                cmd.Parameters.AddWithValue("@pilt", newImagePath); // Сохраняем путь к новому файлу
+                cmd.Parameters.AddWithValue("@kategooria", txtCategory.Text);
+                cmd.ExecuteNonQuery();
+            }
+            LoadData();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                int id = Convert.ToInt32(dataGridView1.SelectedRows[0].Cells["id"].Value);
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+                    SqlCommand cmd = new SqlCommand("DELETE FROM Toode WHERE id = @id", conn);
+                    cmd.Parameters.AddWithValue("@id", id);
+                    cmd.ExecuteNonQuery();
+                }
+                LoadData();
+            }
+            else
+            {
+                MessageBox.Show("Выберите строку для удаления.");
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0) // Проверяем, что кликнули не по заголовку
+            {
+                DataGridViewRow row = dataGridView1.Rows[e.RowIndex];
+
+                // Проверка на null для "pilt"
+                var imagePath = row.Cells["pilt"].Value?.ToString(); // Получаем путь к изображению, если значение не null
+
+                if (!string.IsNullOrEmpty(imagePath) && File.Exists(imagePath))
+                {
+                    using (var imgStream = new FileStream(imagePath, FileMode.Open, FileAccess.Read))
+                    {
+                        pictureBoxPreview.Image = Image.FromStream(imgStream);
+                        pictureBoxPreview.SizeMode = PictureBoxSizeMode.Zoom;
+                    }
+                }
+                else
+                {
+                    pictureBoxPreview.Image = new Bitmap(100, 100); // Заглушка, если файла нет или путь пустой
+                    pictureBoxPreview.SizeMode = PictureBoxSizeMode.Zoom;
+                }
+            }
+        }
+
+        private void SetupDataGridView()
+        {
+            dataGridView1.AutoGenerateColumns = false;
+            dataGridView1.Columns.Clear();
+
+            dataGridView1.Columns.Add(new DataGridViewTextBoxColumn
+            {
+                Name = "id",
+                HeaderText = "ID",
+                DataPropertyName = "id",
+                Visible = false // Скрываем ID
+            });
+
+            dataGridView1.Columns.Add(new DataGridViewTextBoxColumn
+            {
+                Name = "toodenimi",
+                HeaderText = "Название",
+                DataPropertyName = "toodenimi"
+            });
+
+            dataGridView1.Columns.Add(new DataGridViewTextBoxColumn
+            {
+                Name = "kogus",
+                HeaderText = "Количество",
+                DataPropertyName = "kogus"
+            });
+
+            dataGridView1.Columns.Add(new DataGridViewTextBoxColumn
+            {
+                Name = "hind",
+                HeaderText = "Цена",
+                DataPropertyName = "hind"
+            });
+
+            dataGridView1.Columns.Add(new DataGridViewTextBoxColumn
+            {
+                Name = "pilt", // Здесь указываем имя столбца, которое будет отображаться в DataGridView
+                HeaderText = "Изображение",
+                DataPropertyName = "pilt" // Имя столбца в базе данных
+            });
+
+            dataGridView1.Columns.Add(new DataGridViewTextBoxColumn
+            {
+                Name = "kategooria",
+                HeaderText = "Категория",
+                DataPropertyName = "kategooria"
+            });
+
+        }
+
+        private void btnSelectImage_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "Изображения (*.jpg;*.png;*.bmp)|*.jpg;*.png;*.bmp";
+                openFileDialog.Title = "Выберите изображение товара";
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    txtImagePath.Text = openFileDialog.FileName; // Сохраняем путь к файлу
+                    pictureBoxPreview.Image = Image.FromFile(openFileDialog.FileName); // Показываем превью
+                }
+            }
+        }
+    }
+}
